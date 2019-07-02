3743f3a68df291c635df09b47fc429a180f4596c
double <2637309949@qq.com>
2019-07-02 22:37:25 +0800

    feat: 架构调整

--- application.go
+++ application.go
@@ -15,7 +15,6 @@ import (
 
 // Captcha provide Digit Captcha
 type Captcha struct {
-	bulrush.PNBase
 	Period    int
 	Secret    string
 	Config    base64Captcha.ConfigDigit
@@ -40,28 +39,26 @@ func New() *Captcha {
 }
 
 // Plugin for gin
-func (c *Captcha) Plugin() interface{} {
-	return func(cfg *bulrush.Config, router *gin.RouterGroup, httpProxy *gin.Engine) {
-		router.Use(func(ctx *gin.Context) {
-			if data, err := ctx.Cookie("captcha"); err == nil && data != "" {
-				decData := decrypt([]byte(data), c.Secret)
-				dataStr := string(decData)
-				ctx.Set("captcha", dataStr)
-			}
-			ctx.Next()
-		})
-		router.GET(c.URLPrefix, func(ctx *gin.Context) {
-			if height, err := strconv.Atoi(ctx.Query("height")); err != nil && height != 0 {
-				c.Config.Height = height
-			}
-			if width, err := strconv.Atoi(ctx.Query("width")); err != nil && width != 0 {
-				c.Config.Width = width
-			}
-			idKey, captcha := base64Captcha.GenerateCaptcha("", c.Config)
-			encryptData := encrypt([]byte(idKey), c.Secret)
-			base64 := base64Captcha.CaptchaWriteToBase64Encoding(captcha)
-			ctx.SetCookie("captcha", string(encryptData), Some(c.Period, 60).(int), "/", "", false, false)
-			ctx.String(http.StatusOK, base64)
-		})
-	}
+func (c *Captcha) Plugin(cfg *bulrush.Config, router *gin.RouterGroup, httpProxy *gin.Engine) {
+	router.Use(func(ctx *gin.Context) {
+		if data, err := ctx.Cookie("captcha"); err == nil && data != "" {
+			decData := decrypt([]byte(data), c.Secret)
+			dataStr := string(decData)
+			ctx.Set("captcha", dataStr)
+		}
+		ctx.Next()
+	})
+	router.GET(c.URLPrefix, func(ctx *gin.Context) {
+		if height, err := strconv.Atoi(ctx.Query("height")); err != nil && height != 0 {
+			c.Config.Height = height
+		}
+		if width, err := strconv.Atoi(ctx.Query("width")); err != nil && width != 0 {
+			c.Config.Width = width
+		}
+		idKey, captcha := base64Captcha.GenerateCaptcha("", c.Config)
+		encryptData := encrypt([]byte(idKey), c.Secret)
+		base64 := base64Captcha.CaptchaWriteToBase64Encoding(captcha)
+		ctx.SetCookie("captcha", string(encryptData), Some(c.Period, 60).(int), "/", "", false, false)
+		ctx.String(http.StatusOK, base64)
+	})
 }
